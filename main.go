350923a46fb9637940ddc8df358ce1cc27e857c7
harshvardha <harshvardhansingh458@protonmail.com>
2025-05-26 18:26:20 +0530

    checking vulnerabilites

--- main.go
+++ main.go
@@ -95,8 +95,8 @@ func main() {
 
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
+		Addr:              ":" + port,
+		Handler:           router,
 		ReadHeaderTimeout: time.Second * 5,
 	}
 
